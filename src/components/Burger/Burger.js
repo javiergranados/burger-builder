8f29116718527be098133f3feaea7ad9f3738770
jvgranados <jgranadosflores@gmail.com>
2018-07-21 10:48:16 +0200

    Now the ingredients are outputted dynamically

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,13 +4,19 @@ import classes from './Burger.css'
 
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
-const burger = () => {
+const burger = props => {
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map(key => Array(props.ingredients[key]).fill(key).map((ingredient, i) => <BurgerIngredient key={`${ingredient}_${i}`} type={ingredient} />))
+    .reduce((arr, el) => arr.concat(el), [])
+
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>No ingredients added yet!</p>
+  }
+
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type='breadTop' />
-      <BurgerIngredient type='salad' />
-      <BurgerIngredient type='cheese' />
-      <BurgerIngredient type='meat' />
+      {transformedIngredients}
       <BurgerIngredient type='breadBottom' />
     </div>
   )
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,10 +5,19 @@ import Aux from '../../hoc/Aux'
 import Burger from '../../components/Burger/Burger'
 
 class BurgerBuilder extends Component {
+  state = {
+    ingredients: {
+      bacon: 2,
+      salad: 1,
+      cheese: 1,
+      meat: 1
+    }
+  }
+
   render () {
     return (
       <Aux>
-        <Burger />
+        <Burger ingredients={this.state.ingredients} />
         <div>BuildControls</div>
       </Aux>
     )
